d8fea635f630a7219a2ac3b062a7b91f4c122866
IfunanyaWilliam <williamifunanya@gmail.com>
2022-08-02 22:25:58 +0100

    Add interface ICategoryRepository with methods

---
+++ PokemonReviewAPI/Contract/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonReviewAPI.Models;
+
+namespace PokemonReviewAPI.Contract
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetAllCategories();
+        Task<ICollection<Category>> GetAllCategory(int id);
+        Task<ICollection<Pokemon>> GetPokemonByCategory(int categoryId);
+        Task<bool> CategoryExist(int id);
+    }
+}
